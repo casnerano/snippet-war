fedef002009897f6be4d5e049810f2d9cd15b32c
Arutyunyan Arsen T <arsarutyunyan@ozon.ru>
2025-12-27 13:43:34 +0300

    wip

--- backend/migrations/000002_create_users_table.up.sql
+++ backend/migrations/000002_create_users_table.up.sql
@@ -2,7 +2,7 @@
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     telegram_user_id BIGINT NOT NULL UNIQUE,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
 );
 
 -- Add table comment
